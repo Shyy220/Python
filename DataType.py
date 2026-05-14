bcd3d4251b78446cf70cb8801ddc0ca4a9c15bb4
Shyy220 <shyambot20@gmail.com>
2026-05-14 04:17:02 -0500

    Add files via upload

---
+++ DataType.py
@@ -0,0 +1,53 @@
+#Integer
+a = 5
+print(a)
+print(type(a), "\n")
+
+#Float
+b = 1.4
+print(b)
+print(type(b), "\n")
+
+#String
+c = "Hello"
+print (c)
+print(type(c), "\n")
+
+#Boolean
+d = True
+print (d)
+print(type(d), "\n")
+
+
+name = "Penguin"
+age = 15
+is_Student = True
+weight = 38.5
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(is_Student)
+print(type(is_Student))
+
+print(weight)
+print(type(weight))
+
+print("AFTER TYPECASTING" ,  "\n")
+
+i=int(weight)
+print(i)
+print(type(i))
+
+
+k=str(is_Student)
+print(k)
+print(type(k))
+
+l=float(age)
+print(l)
+print(type(l))
+
