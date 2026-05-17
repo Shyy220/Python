8459bba364b67ec71f12b0aa83aef1d7c597c610
Shyy220 <shyambot20@gmail.com>
2026-05-17 02:23:29 -0500

    Add files via upload

---
+++ CelebrationMessage.py
@@ -0,0 +1,9 @@
+First_Word = "Good"
+Second_Word = "Job"
+print(First_Word+ " " +Second_Word)
+
+Message = "Congratulations on getting the gold medal in the state games!!!!"
+print(Message.upper())
+
+Str = "KUDOS"
+print(Str[0:5])
