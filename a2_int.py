96a6967f30497485aa4baece2f556167fe77da13
Shyy220 <shyambot20@gmail.com>
2026-05-21 04:08:09 -0500

    Add files via upload

---
+++ a2_int.py
@@ -0,0 +1,23 @@
+x = 6
+if (type(x) is int):
+      print("True")
+else:
+      print("False")
+x = 6.6
+if (type(x) is float):
+      print("True")
+else:
+      print("False")
+
+
+x=40
+y=40
+
+if x is y:
+            print("X and Y SAME identity")
+else:
+       print("X and Y DIFFERENT identity")
+
+                  
+                
+                  
---
+++ identity.py
@@ -0,0 +1,7 @@
+a = [1,2,3,4,5]
+b = [1,2,3,4,5]
+if a is b:
+    print("They are the same in memory")
+else:
+    print("They are not same in memory")
+
