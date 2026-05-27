5f545538eb2dc75d4a32849e112c57599f10f0e8
Shyy220 <shyambot20@gmail.com>
2026-05-27 04:07:01 -0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,8 @@
+string =input("please enter your own string: ")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\nThe Original String = ", string)
+print("The reversed String=", string2)
