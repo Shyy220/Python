350abb3d93073c6439aee176a73e247e601ac43c
Shyy220 <shyambot20@gmail.com>
2026-05-31 23:43:06 -0500

    Add files via upload

---
+++ age.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+
+    if age <= 20:
+        print("The user is between the age of 10 and 20.")
+    else:
+        print("The user is older than 20.")
+else:
+    print("The user is younger than 10.")
