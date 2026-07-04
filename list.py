f1b717ad9372152783a6f98a4f6fa629745ec64a
Shyy220 <shyambot20@gmail.com>
2026-07-04 08:09:51 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+empty_list = []
+print ()
+
+list = [1,2,3,4,5]
+print(list)
+
+multiply_list = [1,2,3,4,5]*5
+print(multiply_list)
+
+aList = [200,400,600,800,1000]
+aList =aList[::-1]
+print(aList,"\n")
