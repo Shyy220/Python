7cd9c18c53b1ee742d91dc2f4fe45c1bb75eda6d
Shyy220 <shyambot20@gmail.com>
2026-07-05 06:06:22 -0500

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,18 @@
+tuplex = ("tuple", False, 3.2, 3)
+print(tuplex)
+
+tuple = (5,6,7,3,6,7,6,7)
+print(tuple)
+
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (50,60,70,80,50,50,50)
+print(tuple1.count(50))
+
+tuplex = (5,4,3,4,5,3,2,4)
+_slice = tuplex[3:5]
+print(_slice)
+
+tuple2 = (4,5,6,5,4,5,6,5,4,5)
+print(tuplex[:6])
