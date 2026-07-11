5bc2db788da683f8b96289a4eba6ab397d7bc3c6
Shyy220 <shyambot20@gmail.com>
2026-07-11 08:17:21 -0500

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,16 @@
+numbers = {1,2,2,3,3,4,4,5,5,6,6}
+print(numbers)
+
+numbers = {3.2, "Stacy",(1,2,3,4,5)}
+print(numbers)
+
+numbers = {1,2,3,4,5,4,3,2,1,2,3,4,5,6,7,8,3,2,3,4}
+print(numbers)
+
+value = set([1,2,3,4,5,6,5])
+print(value)
+
+numbers = {1,2,3,4,5}
+numbers.pop()
+print(numbers)
+
